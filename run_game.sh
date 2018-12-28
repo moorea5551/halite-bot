ad17caadce1eb8af338d7d5b0b9bb6b773b60225
Andrew Moore <andrew.moore@navihealth.com>
2018-12-28 13:40:29 -0600

    run game actually works now

--- run_game.sh
+++ run_game.sh
@@ -3,4 +3,4 @@
 set -e
 go build
 
-./halite --replay-directory replays/ -vvv --width 32 --height 32 "go run main" "go run main"
+./halite --replay-directory replays/ -vvv --width 32 --height 32 "go run main.go" "go run main.go"
